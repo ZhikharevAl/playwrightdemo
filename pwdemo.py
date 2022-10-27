8ed97bf430a2e01c6bf1dac870180516bfa42e0b
AlexeyZh <waltafunk@gmail.com>
2022-10-27 23:07:56 +0300

    #login-button

--- pwdemo.py
+++ pwdemo.py
@@ -3,3 +3,6 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=False, slow_mo=50)
     page = browser.new_page()
     page.goto("https://www.saucedemo.com/")
+    page.fill("#user-name", "standard_user")
+    page.fill("#password", "secret_sauce")
+    page.click("#login-button")
